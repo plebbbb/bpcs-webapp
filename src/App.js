b05b7e628b41d42d4844bf605bcc019e8e390304
plebbbb <63123021+plebbbb@users.noreply.github.com>
2022-05-05 16:36:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import { Component } from 'react';
 class App extends Component{
   render(){
     return(
-      <div class = "flex">
-        <Encode/>
-        <Decode/>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+        <h1>BPCS Steganography Webapp<h1/>
+        <div maxwidth=50vw>
+             "BPCS is an image steganography algorithm which attempts to hide data within images while minimizing any visual artifacting caused from the encryption.
+             This is done via a technique to determine the uniformitivity of the image in order to estimate the visual disturbance caused by the change.
+             Encode and decode files below:"
+        <div/>
+        <div class = "flex">
+          <Encode/>
+          <Decode/>
+        </div
       </div>
     )
   }
